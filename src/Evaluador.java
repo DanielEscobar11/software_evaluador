f731396fb91a6173b4c520cb18c548176000332a
DanielEscobar11 <drei100505@gmail.com>
2026-07-10 19:37:40 -0600

    Cambio

---
+++ src/Evaluador.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TareaCorta1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int opcion = 0;
+        double x, y, resultado;
